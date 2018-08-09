88472911335f10faac04f248aa2c6a4a86c41905
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-09 12:56:36 +0530

    Create Numsepareate.java

---
+++ Numsepareate.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Numsepareate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char[] a=sc.next().toCharArray();
+		//s=sc.nextLine();
+		for(int i=0;i<a.length;i++) 
+		{
+			System.out.print(a[i]+" ");
+     	}
+		
+		
+	}
+}
